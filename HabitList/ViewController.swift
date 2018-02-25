bc815717f3e439da6522fded52a728b35e6c8c49
Yuya Imoto <yuya@yuya-MacBookPro.local>
2018-02-25 10:45:24 +0900

    追加ボタンの実装

--- HabitList/ViewController.swift
+++ HabitList/ViewController.swift
@@ -13,15 +13,25 @@ class ViewController: UIViewController, UITableViewDataSource {
     // MARK: - Properties
 
     @IBOutlet weak var taskTableView: UITableView!
+    var plusButton: UIBarButtonItem?
+    var strings = [String]()
 
     // MARK: - View Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //データの準備
+        self.strings.append("aaa")
+        self.strings.append("bbb")
+        self.strings.append("ccc")
+        self.strings.append("ddd")
+        self.strings.append("eee")
         self.taskTableView.dataSource = self
         // ナビゲーションバーに編集ボタンを追加
         self.navigationItem.setRightBarButton(self.editButtonItem, animated: true)
+        //追加ボタンをプロパティとして持つ
+        self.plusButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(plusButtonTapped(_:)))
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,13 +43,13 @@ class ViewController: UIViewController, UITableViewDataSource {
 
     // セル数を決める
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return self.strings.count
     }
 
     // セルの内容を決める
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "section:[\(indexPath.section)], row:[\(indexPath.row)]"
+        cell.textLabel?.text = self.strings[indexPath.row]
         return cell
     }
 
@@ -48,6 +58,32 @@ class ViewController: UIViewController, UITableViewDataSource {
 
         // 自分が持っているテーブルビューのeditingを更新する
         self.taskTableView.setEditing(editing, animated: animated)
+
+        // 通常・編集モードの切り替え
+        if editing {
+            //プラスボタンをナビゲーションバーの左側へ表示させる。
+            self.navigationItem.setLeftBarButton(self.plusButton, animated: true)
+        } else {
+            self.navigationItem.setLeftBarButton(nil, animated: true)
+        }
+    }
+
+    @objc func plusButtonTapped(_ sender: Any) {
+        //あらかじめデータソースを編集しておく。
+        self.strings.insert("added content", at: 0)
+
+        //テーブルビュー挿入開始
+        self.taskTableView.beginUpdates()
+
+        //挿入するIndexPath
+        var paths = [IndexPath]()
+        paths.append(IndexPath(row: 0, section: 0))
+
+        //挿入処理
+        self.taskTableView.insertRows(at: paths, with: .automatic)
+
+        //テーブルビュー挿入終了
+        self.taskTableView.endUpdates()
     }
 }
 
